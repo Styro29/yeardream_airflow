21053145c3ba73f081e39570d63b50fa8655b224
wooyeol.park <twinis@gmail.com>
2024-06-14 17:53:31 +0900

    select_fruit.sh 3차 수정

--- plugins/shell/select_fruit.sh
+++ plugins/shell/select_fruit.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!usr/bin/bash
 FRUIT=$1
-if $FRUIT == "APPLE";then
+if [$FRUIT == APPLE];then
 	echo "You selected Apple!"
-elif $FRUIT == "ORANGE";then
+elif [$FRUIT == ORANGE];then
 	echo "You selected Orange!"
-elif $FRUIT == "GRAPE";then
+elif [$FRUIT == GRAPE];then
 	echo "You selected Grape!"
 else
 	echo "You selected other Fruit!"
